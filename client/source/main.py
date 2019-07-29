f13f351fa3a79e6369fef8b78458c9f2a5f251ae
harshsikhwal <harshsikhwal7@gmail.com>
2019-07-29 10:51:46 +0530

    Adding main

--- client/source/main.py
+++ client/source/main.py
@@ -4,42 +4,46 @@ from initializer import *
 
 from logger import *
 
+def main():
+    # filling up the logs
 
-# filling up the logs
+    # IF NOTHING IS PRESENT THEN CREATE FOLDERS, FILES AND SO ON
 
-# IF NOTHING IS PRESENT THEN CREATE FOLDERS, FILES AND SO ON
+    # Read the connections from connection_info.ini and initialize the connection objects
 
-# Read the connections from connection_info.ini and initialize the connection objects
+    initialize_config_file()
 
-initialize_config_file()
+    constant.info_logger = Logger('i')
 
-constant.info_logger = Logger('i')
+    constant.error_logger = Logger('e')
 
-constant.error_logger = Logger('e')
+    constant.info_logger.write_info('i', "Main Start!")
 
-constant.info_logger.write_info('i', "Main Start!")
+    constant.error_logger.write_info('i', "Main Start!")
 
-constant.error_logger.write_info('i', "Main Start!")
+    constant.info_logger.write_info('i', "Initializing Connections\tStart")
 
-constant.info_logger.write_info('i', "Initializing Connections\tStart")
+    initialize_connections()
 
-initialize_connections()
+    constant.info_logger.write_info('i', "Initializing Connections\tEnd")
 
-constant.info_logger.write_info('i', "Initializing Connections\tEnd")
+    # After initialization, read the particular file where destination and source is present and map it accordingly
 
-# After initialization, read the particular file where destination and source is present and map it accordingly
+    constant.info_logger.write_info('i', "Source and Destination locations\tStart")
 
-constant.info_logger.write_info('i', "Source and Destination locations\tStart")
+    initialize_src_dest()
 
-initialize_src_dest()
+    # closing the logs at the last after all write operations has been done
 
-# closing the logs at the last after all write operations has been done
+    constant.info_logger.write_info('i', "Main End!")
 
-constant.info_logger.write_info('i', "Main End!")
+    constant.error_logger.write_info('i', "Main End!")
 
-constant.error_logger.write_info('i', "Main End!")
+    constant.info_logger.file_fp.close()
+    constant.error_logger.file_fp.close()
 
-constant.info_logger.file_fp.close()
-constant.error_logger.file_fp.close()
+    # print("\n Finished !! \n")
 
-# print("\n Finished !! \n")
+
+if __name__ == "__main__":
+    main()
